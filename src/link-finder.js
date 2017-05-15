c70f2384a490a47aa050d3825225a076d880be72
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-15 03:12:07 +0000

    Switch to snekfetch

--- src/link-finder.js
+++ src/link-finder.js
@@ -1,11 +1,16 @@
-const request = require('superagent');
+const snekfetch = require('snekfetch');
 const cheerio = require('cheerio');
 
 module.exports = (name, options = {}) => {
     if(!options.size) options.size = 5;
     if(!options.mood) options.mood = 1;
-    return request
-        .get(`http://www.sunnyneo.com/petimagefinder.php?name=${name}&size=${options.size}&mood=${options.mood}`)
+    return snekfetch
+        .get('http://www.sunnyneo.com/petimagefinder.php')
+        .query({
+            name,
+            size: options.size,
+            mood: options.mood
+        })
         .then(response => {
             const $ = cheerio.load(response.text);
             const link = $('textarea').first().text();
