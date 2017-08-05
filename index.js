6edf4b766252cf9eb2756205d5165c1c3c4e9bda
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-08-05 20:16:13 +0000

    Remove cheerio

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const snekfetch = require('snekfetch');
-const cheerio = require('cheerio');
 
 module.exports = (name, { size, mood } = {}) => snekfetch
-    .get('http://www.sunnyneo.com/petimagefinder.php')
-    .query({
-        name,
-        size: size || 5,
-        mood: mood || 1
-    })
-    .then((res) => {
-        const $ = cheerio.load(res.text);
-        return $('textarea').first().text();
-    });
+	.get('http://www.sunnyneo.com/petimagefinder.php')
+	.query({
+		name,
+		size: size || 5,
+		mood: mood || 1
+	})
+	.then(res => {
+		const link = res.text.match(/http:\/\/pets\.neopets\.com\/cp\/.+\.png/);
+		if (!link) throw new Error('Invalid Pet Name.');
+		return link[0];
+	});
