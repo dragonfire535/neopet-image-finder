c6b0b6d55c55b2730c7c65fe786fb66c88304932
bdistin <bdistin@gmail.com>
2017-10-19 19:54:04 -0500

    Trade 8 char for 54 less
    simplifies the code a bit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const snekfetch = require('snekfetch');
 
-module.exports = (name, { size, mood } = {}) => {
+module.exports = (name, { size = 5, mood = 1 } = {}) => {
 	if (typeof name !== 'string') throw new TypeError('name must be a string.');
-	if (size && typeof size !== 'number') throw new TypeError('size must be a number or falsey.');
-	if (size && (size < 1 || size > 5)) throw new RangeError('size must be a number between 1 and 5.');
-	if (mood && typeof mood !== 'number') throw new TypeError('mood must be a number or falsey.');
-	if (mood && (mood < 1 || mood > 5)) throw new RangeError('mood must be a number between 1 and 5.');
+	if (typeof size !== 'number') throw new TypeError('size must be a number or falsey.');
+	if (size < 1 || size > 5) throw new RangeError('size must be a number between 1 and 5.');
+	if (typeof mood !== 'number') throw new TypeError('mood must be a number or falsey.');
+	if (mood < 1 || mood > 5) throw new RangeError('mood must be a number between 1 and 5.');
 	return snekfetch
 		.get('http://www.sunnyneo.com/petimagefinder.php')
 		.query({
 			name,
-			size: size || 5,
-			mood: mood || 1
+			size,
+			mood
 		})
 		.then(({ text }) => {
 			const link = text.match(/http:\/\/pets\.neopets\.com\/cp\/.+\.png/);
