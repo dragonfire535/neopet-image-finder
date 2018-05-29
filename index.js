fe2f6417ee09cf543ccf912e495bc4c728af7909
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-05-29 21:58:25 +0000

    Make Errors better

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const snekfetch = require('snekfetch');
 
 module.exports = (name, { size = 5, mood = 1 } = {}) => {
 	if (typeof name !== 'string') throw new TypeError('name must be a string.');
-	if (typeof size !== 'number') throw new TypeError('size must be a number or falsey.');
-	if (size < 1 || size > 7) throw new RangeError('size must be a number between 1 and 7.');
-	if (typeof mood !== 'number') throw new TypeError('mood must be a number or falsey.');
-	if (mood < 1 || mood > 5) throw new RangeError('mood must be a number between 1 and 5.');
+	if (typeof size !== 'number' || size < 1 || size > 7) throw new TypeError('size must be a number from 1-7.');
+	if (typeof mood !== 'number' || mood < 1 || mood > 5) throw new TypeError('mood must be a number from 1-5.');
 	return snekfetch
 		.get('http://www.sunnyneo.com/petimagefinder.php')
 		.query({ name, size, mood })
 		.then(({ raw }) => {
 			const link = raw.toString().match(/http:\/\/pets\.neopets\.com\/cp\/.+\.png/);
-			if (!link) throw new Error('Invalid Neopet name.');
+			if (!link) throw new Error(`Could not find a Neopet with the name ${name}.`);
 			return link[0];
 		});
 };
