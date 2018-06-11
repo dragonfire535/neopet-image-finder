b56fa3ac6070843a108d731836948d7d87a03f71
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-06-11 17:33:28 +0000

    Switch to node-superfetch

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const snekfetch = require('snekfetch');
+const request = require('node-superfetch');
 
 module.exports = (name, { size = 5, mood = 1 } = {}) => {
 	if (typeof name !== 'string') throw new TypeError('name must be a string.');
 	if (typeof size !== 'number' || size < 1 || size > 7) throw new TypeError('size must be a number from 1-7.');
 	if (typeof mood !== 'number' || mood < 1 || mood > 5) throw new TypeError('mood must be a number from 1-5.');
-	return snekfetch
+	return request
 		.get('http://www.sunnyneo.com/petimagefinder.php')
 		.query({ name, size, mood })
-		.then(({ raw }) => {
-			const link = raw.toString().match(/http:\/\/pets\.neopets\.com\/cp\/.+\.png/);
+		.then(({ text }) => {
+			const link = text.match(/http:\/\/pets\.neopets\.com\/cp\/.+\.png/);
 			if (!link) throw new Error(`Could not find a Neopet with the name ${name}.`);
 			return link[0];
 		});
